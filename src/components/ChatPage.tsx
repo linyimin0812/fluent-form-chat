91706822b5d04a2a983a1d561964d74ba5e89057
linyimin <linyimin520812@gmail.com>
2025-06-24 10:31:50 +0800

    fix: avoid blank section

--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -199,7 +199,7 @@ const ChatPage = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col bg-gray-50 dark:bg-gray-900">
       {/* Header */}
       <div className="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 p-4 flex-shrink-0 sticky top-0 z-20">
         <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
